16a0aee77c63940d12aaa852f9da06f5467d5f60
Leo Cheung <leocmcheung@gmail.com>
2022-09-12 12:23:23 +0100

    Added some placeholder texts on both pages

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from re import S
 import streamlit as st
 
-st.title("TEST")
+st.title("Forecasting carbon emissions to limit global warming")
 
-st.write("SOMETHING")
+st.write("Some texts on global warming")
+
+st.write("Some texts on our methodology")
+
+st.write("Some explanation on our model")
--- pages/model.py
+++ pages/model.py
@@ -10,7 +10,7 @@ import joblib
 
 filepath = os.path.abspath("model/X_2.csv")
 modelpath = os.path.abspath("model/test3.sav")
-st.title("Another page")
+st.title("Predict how much annual carbon emission your company has")
 
 ## load pickle
 # loaded = XGBRegressor()
@@ -19,12 +19,19 @@ st.title("Another page")
 model = XGBRegressor()
 model.load_model("model/test.json")
 # loaded = joblib.load(modelpath)
+st.write("This is our model:")
 st.write(type(model))
 ## enter input as boxes ideally but can just use one line of our data first
 X_test = pd.read_csv(filepath)
+st.write("A random row from our X_test_transform")
+st.write("(Maybe we should do PCA on the data anyway to cover up the numbers and\
+         keep the data inacessible even if leaked?)")
 X_2 = np.array(X_test).reshape(1,468)
 st.write(X_2)
 ## do predict
+st.write("Here is the prediction for our random row:")
 result = model.predict(X_2)
 ## print output
-st.write(result)
+st.write(result[0])
+st.write("... and this is the predicted Carbon Intensity for your company (Scope 1\
+    and Scope 2 included)")
